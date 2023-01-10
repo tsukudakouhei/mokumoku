276a68e0a6b923c8db379b7aad40bf3bf74bf6d1
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-01-10 23:11:39 +0900

    Add: genderカラムを追加、ユーザ登録時にgenderを選択可能に

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+  enum gender: { other: 0, woman: 1, man: 2 }
 
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
@@ -75,5 +76,9 @@ class User < ApplicationRecord
 
   def allow_liked_event_notification?
     notification_timings.liked_event.present?
+  end
+
+  def only_woman_join?(event)
+    event.only_woman && woman?
   end
 end
